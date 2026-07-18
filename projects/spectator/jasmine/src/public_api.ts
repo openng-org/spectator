7e6de4dd59994598218f2654ebeee52b9d3feeea
Alanoll <alanollv@gmail.com>
2026-07-18 10:54:15 -0500

    wip: fix matcher-types regressiosn from rebase

--- projects/spectator/jasmine/src/public_api.ts
+++ projects/spectator/jasmine/src/public_api.ts
@@ -1,11 +1,13 @@
-/// <reference path="./lib/matchers-types.ts" preserve="true" />
+import './lib/matchers-types';
+
 export * from './lib/dom-selectors';
 export * from './lib/mock';
 export * from './lib/spectator';
-export * from './lib/spectator-http';
 export * from './lib/spectator-directive';
-export * from './lib/spectator-service';
 export * from './lib/spectator-host';
-export * from './lib/spectator-routing';
-export * from './lib/spectator-pipe';
+export * from './lib/spectator-http';
 export * from './lib/spectator-injection-context';
+export * from './lib/spectator-pipe';
+export * from './lib/spectator-routing';
+export * from './lib/spectator-service';
+
